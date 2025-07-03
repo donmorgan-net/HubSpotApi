e19d6ec61ce0b7e192e84e465f3270256681c248
Don Morgan <donm@donmorgan.net>
2025-07-03 12:45:02 -0500

    Removed troubleshooting code in publish script

--- .github/Publish.ps1
+++ .github/Publish.ps1
@@ -5,8 +5,6 @@ param(
 #Workaround for known issue: https://github.com/PowerShell/PSResourceGet/issues/1806
 Get-PSResourceRepository | out-null
 
-Write-Host $PSScriptRoot
-Write-Host (Get-Location)
 
 $ModulePath = ".\Module\$TargetFolder"
 Publish-PSResource -Path $ModulePath -ApiKey $Env:APIKEY
