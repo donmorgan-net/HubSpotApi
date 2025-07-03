074146f875d46f9787fec61cea3699d127cafec9
Don Morgan <donm@donmorgan.net>
2025-07-03 12:39:39 -0500

    troubleshootion action working dir

--- .github/Publish.ps1
+++ .github/Publish.ps1
@@ -5,5 +5,7 @@ param(
 #Workaround for known issue: https://github.com/PowerShell/PSResourceGet/issues/1806
 Get-PSResourceRepository | out-null
 
-$ModulePath = "$PSScriptRoot\Module\$TargetFolder"
+Write-Host $PSScriptRoot
+
+$ModulePath = "..\Module\$TargetFolder"
 Publish-PSResource -Path $ModulePath -ApiKey $Env:APIKEY
